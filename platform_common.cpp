77ae981ba8bad40d6404dc81f77713544af4c649
Taher Saraf <tahersaraf98@gmail.com>
2024-08-16 21:40:51 +0530

    Adding all local files

---
+++ platform_common.cpp
@@ -0,0 +1,18 @@
+struct Button_State {
+	bool is_down;
+	bool changed;
+};
+
+enum {
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_W,
+	BUTTON_S,
+	BUTTON_ENTER,
+
+	BUTTON_COUNT //Should be last item
+};
+
+struct Input {
+	Button_State buttons[BUTTON_COUNT];
+};
